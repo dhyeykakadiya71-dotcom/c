05e2ba018b78be97c1c74586eea04044a2dec52a
dhyeykakadiya71-dotcom <dhyeykakadiya71@gmail.com>
2026-09-24 15:06:35 +0530

    Add basic arithmetic operations in program2.cpp

---
+++ c-prt6/program2.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int a = 15, b = 4;
+    printf("a + b = %d\n", a + b);
+    printf("a - b = %d\n", a - b);
+    printf("a * b = %d\n", a * b);
+    printf("a / b = %d\n", a / b);
+    printf("a %% b = %d\n", a % b);
+    return 0;
+}
